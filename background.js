7baab2ade8d7c708a56ecd5fa8b9848573038d7f
Juxuny Wu <juxuny@gmail.com>
2024-10-08 01:55:36 +0800

    keep Ctrl+W working in other tabs where xterm-helper-textarea is not found

--- background.js
+++ background.js
@@ -1,17 +1,30 @@
+function SendKeyboradEvent() {
+  if (document.activeElement.className == "xterm-helper-textarea"){
+    document.activeElement.dispatchEvent(new KeyboardEvent("keydown", {key: "w", keyCode: 87, ctrlKey: true}));
+    return true;
+  }
+  return false;
+}
+
 chrome.commands.onCommand.addListener(function(command) {
   if (command === 'captured') {
     console.log('Ctrl+W shortcut triggered');
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      if (tabs[0].url.indexOf("chrome://") === 0) {
+        chrome.tabs.remove(tabs[0].id);
+        return;
+      }
       chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
           function: SendKeyboradEvent
+      }).then(injectionResult => {
+        for (const { result } of injectionResult) {
+          if (!result) {
+            chrome.tabs.remove(tabs[0].id);
+          }
+        }
       });
     });
   }
 });
 
-function SendKeyboradEvent(){
-  if (document.activeElement.className == "xterm-helper-textarea"){
-    document.activeElement.dispatchEvent(new KeyboardEvent("keydown", {key: "w", keyCode: 87, ctrlKey: true}));
-  }
-}
