3d4140b2c4fc997b7b818e6bc3c9d1e1e39ad8c1
Juxuny Wu <juxuny@gmail.com>
2024-10-08 15:21:31 +0800

    capture ctrl+n & ctrl+q

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-function SendKeyboradEvent() {
+function SendCtrlW() {
   if (document.activeElement.className == "xterm-helper-textarea"){
     document.activeElement.dispatchEvent(new KeyboardEvent("keydown", {key: "w", keyCode: 87, ctrlKey: true}));
+    return true;
+  }
+  return false;
+}
+
+function SendCtrlN() {
+  if (document.activeElement.className == "xterm-helper-textarea"){
+    document.activeElement.dispatchEvent(new KeyboardEvent("keydown", {key: "n", keyCode: 78, ctrlKey: true}));
     return true;
   }
   return false;
@@ -10,17 +18,38 @@ chrome.commands.onCommand.addListener(function(command) {
   if (command === 'captured') {
     console.log('Ctrl+W shortcut triggered');
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      if (tabs[0].url.indexOf("chrome://") === 0) {
+      console.log(tabs[0]);
+      if (tabs[0].url.indexOf("http") < 0) {
         chrome.tabs.remove(tabs[0].id);
         return;
       }
       chrome.scripting.executeScript({
           target: { tabId: tabs[0].id },
-          function: SendKeyboradEvent
+          function: SendCtrlW
       }).then(injectionResult => {
         for (const { result } of injectionResult) {
           if (!result) {
             chrome.tabs.remove(tabs[0].id);
+          }
+        }
+      });
+    });
+  }
+  if (command === 'captured-new-tab') {
+    console.log('Ctrl+N shortcut triggered');
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      console.log(tabs[0]);
+      if (tabs[0].url.indexOf("http") < 0) {
+        chrome.tabs.create({ url: "" });
+        return;
+      }
+      chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: SendCtrlN
+      }).then(injectionResult => {
+        for (const { result } of injectionResult) {
+          if (!result) {
+            chrome.tabs.create({ url: "" });
           }
         }
       });
